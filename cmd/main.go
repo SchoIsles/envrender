938735c7a300ac65a3343086f77e68a57e7e3b53
SchoIsles <okletswin@gmail.com>
2024-10-23 11:05:16 +0800

    Add taskfile

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,62 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/SchoIsles/envtpl"
 )
 
+type Options struct {
+	File string
+	Out  string
+}
+
 func main() {
-	tpl := `
-echo PATH: {{ .PATH }}
-current: {{ now | date "2006-01-02" }}
-`
-	res, err := envtpl.Render(tpl)
-	if err != nil {
-		panic(err)
+	opt := &Options{}
+	flags := flag.NewFlagSet("envtpl", flag.ExitOnError)
+	flags.StringVar(&opt.File, "f", "", "Input file path")
+	flags.StringVar(&opt.Out, "o", "", "Output file path")
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	fmt.Println(res)
+
+	var output io.Writer
+	if opt.Out != "" {
+		file, err := os.OpenFile(opt.Out, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			exitErr(err)
+		}
+		output = file
+		defer file.Close()
+	} else {
+		output = os.Stdout
+	}
+
+	if opt.File != "" && opt.File != "-" {
+		err := envtpl.RenderFileWithWriter(output, opt.File)
+		if err != nil {
+			exitErr(err)
+		}
+	} else {
+		var input bytes.Buffer
+		_, err := io.Copy(&input, os.Stdin)
+		if err != nil {
+			exitErr(err)
+		}
+		err = envtpl.RenderWithWriter(output, input.String())
+		if err != nil {
+			exitErr(err)
+		}
+	}
+
+}
+
+func exitErr(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
 }
